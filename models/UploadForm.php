715edf2c53c307a1b4bf02d029690a412425389b
makhovdmitrii <makhovdmitrii@gmail.com>
2023-02-18 16:36:34 +0300

    Making lover transliterated and unique filenames on duplication.

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -11,6 +11,15 @@ class UploadForm extends Model
      * @var UploadedFile[]
      */
     public $imageFiles;
+    private $converter = array(
+        'а' => 'a',    'б' => 'b',    'в' => 'v',    'г' => 'g',    'д' => 'd',
+        'е' => 'e',    'ё' => 'e',    'ж' => 'zh',   'з' => 'z',    'и' => 'i',
+        'й' => 'y',    'к' => 'k',    'л' => 'l',    'м' => 'm',    'н' => 'n',
+        'о' => 'o',    'п' => 'p',    'р' => 'r',    'с' => 's',    'т' => 't',
+        'у' => 'u',    'ф' => 'f',    'х' => 'h',    'ц' => 'c',    'ч' => 'ch',
+        'ш' => 'sh',   'щ' => 'sch',  'ь' => '',     'ы' => 'y',    'ъ' => '',
+        'э' => 'e',    'ю' => 'yu',   'я' => 'ya',
+    );
 
     public function rules()
     {
@@ -23,6 +32,13 @@ class UploadForm extends Model
     {
         if ($this->validate()) {
             foreach ($this->imageFiles as $file) {
+                $file->name = mb_strtolower($file->name);
+                $file->name = strtr($file->name, $this->converter);
+                if (file_exists('uploads/' . $file->baseName . '.' . $file->extension))
+                {
+                    //Duplication. Making unique filename.
+                    $file->name = $file->baseName . date('Y_m_d_H_i_s') . '.' . $file->extension;
+                }
                 $file->saveAs('uploads/' . $file->baseName . '.' . $file->extension);
             }
             return true;
