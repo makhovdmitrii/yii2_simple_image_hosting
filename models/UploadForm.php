6e4f55e3647d36ffc390bfc79bb63cf509c69670
makhovdmitrii <makhovdmitrii@gmail.com>
2023-02-23 18:16:02 +0300

    image gallery with resampled images. Supported 2 types of image file formats.

--- models/UploadForm.php
+++ models/UploadForm.php
@@ -44,10 +44,49 @@ class UploadForm extends Model
                 $uploadedFile = new Uploadedfiles();
                 $uploadedFile->name = $file->name;
                 $uploadedFile->save();
+                switch ($file->extension)
+                {
+                    case 'png':
+                        $this::saveMiniImagePNG('uploads/' . $file->baseName . '.' . $file->extension,'minified/' . $file->baseName . '.' . $file->extension);
+                        break;
+                    case 'jpg':
+                    case 'jpeg':
+                        $this::saveMiniImageJPEG('uploads/' . $file->baseName . '.' . $file->extension,'minified/' . $file->baseName . '.' . $file->extension);
+                        break;
+                    default:
+                        echo "There is no supported image type";
+                        break;
+                }
             }
             return true;
         } else {
             return false;
         }
+    }
+    private static function saveMiniImagePNG($srcFile, $destFile)
+    {
+        $size = getimagesize($srcFile);
+        $im = imagecreatefrompng($srcFile);
+
+        $w = 400;
+        $h = $w * $size[1] / $size[0];
+
+        $imf = imagecreatetruecolor($w, $h);
+        imagecopyresampled($imf, $im, 0,0,0,0, $w,$h,$size[0],$size[1]);
+
+        imagePng($imf, $destFile);
+    }
+    private static function saveMiniImageJPEG($srcFile, $destFile)
+    {
+        $size = getimagesize($srcFile);
+        $im = imagecreatefromjpeg($srcFile);
+
+        $w = 400;
+        $h = $w * $size[1] / $size[0];
+
+        $imf = imagecreatetruecolor($w, $h);
+        imagecopyresampled($imf, $im, 0,0,0,0, $w,$h,$size[0],$size[1]);
+
+        imagejpeg($imf, $destFile);
     }
 }
